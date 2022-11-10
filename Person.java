c74462b8a9da9e7df58bbf31664e143a8fc3df6c
Michelle L <mlawson@smith.edu>
2022-11-10 15:55:41 -0500

    more comments

--- Person.java
+++ Person.java
@@ -1,9 +1,12 @@
+import java.util.ArrayList;
 public class Person implements Contract {
     Inventory myInventory;// person's personal inventory
     Boolean awake;//whether the person is awake or not
     String name;//name of the person
     String[] size;//size of the person
     int[] coordinates;//coordinates of the person
+    String lastAction;//last action taken by person
+    ArrayList<String> undoableActions;
 
 
     /**
@@ -15,6 +18,15 @@ public class Person implements Contract {
         this.name = name;
         this.size = new String[] {"small", "medium", "large"};
         this.coordinates = new int[] {0, 0, 0};
+        this.lastAction = "spawned";
+        this.undoableActions = new ArrayList<String>();//list of actions that can be undone
+            undoableActions.add("grab");
+            undoableActions.add("drop");
+            undoableActions.add("rest");
+            undoableActions.add("wake");
+            undoableActions.add("walk");
+            undoableActions.add("grow");
+            undoableActions.add("shrink");
     }
 
     
@@ -23,9 +35,10 @@ public class Person implements Contract {
      * @param item item being grabbed
      */
     public void grab(String item){
-        if (this.myInventory.content.containsKey(item)){
-            this.myInventory.content.put(item, this.myInventory.content.get(item) + 1);
-        } else {
+        if (this.myInventory.content.containsKey(item)){//if the item is already in the inventory, increase the quantity
+            this.myInventory.content.put(item, this.myInventory.content.get(item) + 1);//
+            this.lastAction = "grab "+item;//update last action
+        } else {//if the item is not in the inventory, add it
             this.myInventory.content.put(item, 1);
         }
     }
@@ -37,12 +50,14 @@ public class Person implements Contract {
      * @return String declaring item being dropped
      */
     public String drop(String item){
-        if (this.myInventory.content.containsKey(item)){
+        if (this.myInventory.content.containsKey(item)){//if the item is in the inventory, reduce quantity or remove it
             if (this.myInventory.content.get(item) > 1){
                 this.myInventory.content.put(item, this.myInventory.content.get(item) - 1);
+                this.lastAction = "drop " +item;//update last action
                 return "You dropped " + item + ".";
             } else {
                 this.myInventory.content.remove(item);
+                this.lastAction = "drop "+item;
                 return "You dropped " + item + ".";
             }
         }
@@ -58,19 +73,23 @@ public class Person implements Contract {
      * @return boolean whether the person moved or not
      */
     public boolean walk(String direction){
-        if (direction.equals("north")){
+        if (direction.equals("north")){//if the person is moving north, increase y coordinate
             this.coordinates[1] += 1;
+            this.lastAction = "walk north";
             return true;
-        } else if (direction.equals("south")){
+        } else if (direction.equals("south")){//if the person is moving south, decrease y coordinate
             this.coordinates[1] -= 1;
+            this.lastAction = "walk south";
             return true;
-        } else if (direction.equals("east")){
+        } else if (direction.equals("east")){//if the person is moving east, increase x coordinate
             this.coordinates[0] += 1;
+            this.lastAction = "walk east";
             return true;
-        } else if (direction.equals("west")){
+        } else if (direction.equals("west")){//if the person is moving west, decrease x coordinate
             this.coordinates[0] -= 1;
+            this.lastAction = "walk west";
             return true;
-        } else {
+        } else {//if the person cannot move, return false
             System.out.println("Invalid direction");
             return false;
         }
@@ -80,6 +99,7 @@ public class Person implements Contract {
      * lets person teleport to a different location
      * @param x x coordinate
      * @param y y coordinate
+     * @return T/F : Has the person just flown?
      */
     public boolean fly(int x, int y){
         this.coordinates[2] = 1;//person is now flying
@@ -88,75 +108,116 @@ public class Person implements Contract {
         this.coordinates[1] = y;
         this.coordinates[2] = 0;//person is now on the ground
         System.out.println("You have landed at " + this.coordinates[0] + ", " + this.coordinates[1]);
+        return true; // person has just flown
     }
 
     /**
      * changes current awake status to not awake
      */
-    private void rest(){
-        if (this.awake == true){
+    public void rest(){
+        if (this.awake == true){//if the person is awake, change to not awake
             this.awake = false;
+            System.out.println("Good night! You're sleeping now!");
+            System.out.println("Zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz");
+            this.lastAction ="rest";
         } else {
             System.out.println("You are already asleep");
         }
     }
 
-
-    private void wake(){
-        if (this.awake == false){
+    /**
+     * wakes person up
+     */
+    public void wake(){
+        if (this.awake == false){//if the person is not awake, change to awake
             this.awake = true;
+            System.out.println("Good morning! You're awake now!");
+            this.lastAction = "wake";
         } else {
             System.out.println("You are already awake");
         }
     }
 
-    private void undo(){
-        System.out.println("Previous action undone!");
+    /**
+     * reduce size
+     * @return number of sizes reduced
+     */
+    public Number shrink(){
+        if (this.size[0].equals("small")){//if the person is already small, do nothing
+            System.out.println("You are already at your smallest size!");
+            return 0;
+        } else if (this.size[0].equals("medium")){//if the person is medium, change to small
+            this.size[0] = "small";
+            System.out.println("You are now small!");
+            return 1;
+        } else if (this.size[0].equals("large")){//if the person is large, change to medium
+            System.out.println("lastAction");
+            this.size[0] = "medium";
+            System.out.println("You are now medium!");
+            return 1;
+        }
+        else {
+            System.err.println("Invalid size");
+            return 0;
+        }
+    }
+
+    /**
+     * increase size
+     * @return number of sizes increased
+     */
+    public Number grow(){
+        if (this.size[0].equals("large")){
+            System.out.println("You are already at your largest size!");
+            return 0;
+        } else if (this.size[0].equals("medium")){
+            this.size[0] = "large";
+            System.out.println("You are now large!");
+            return 1;
+        } else if (this.size[0].equals("small")){
+            this.size[0] = "medium";
+            System.out.println("You are now medium!");
+            return 1;
+        }
+        else{
+            System.err.println("Invalid size");
+            return 0;
+        }
+    }
+
+    /**
+     * 
+     */
+    public void undo(){
+        // if (this.lastAction in [""]){
+        //     System.out.println("Previous action undone!");
+        // }
+        
     }
 
     
     /** 
      * @param item
      */
-    private void use(String item){
+    public void use(String item){
         if (this.myInventory.content.containsKey(item)){
             this.myInventory.content.remove(item);
         }
     }
 
-    private void shrink(){
-        if (this.size[0].equals("small")){
-            System.out.println("You are already at your smallest size!");
-        } else if (this.size[0].equals("medium")){
-            this.size[0] = "small";
-        } else if (this.size[0].equals("large")){
-            this.size[0] = "medium";
-        }
-    }
-
-    private void grow(){
-        if (this.size[0].equals("large")){
-            System.out.println("You are already at your largest size!");
-        } else if (this.size[0].equals("medium")){
-            this.size[0] = "large";
-        } else if (this.size[0].equals("small")){
-            this.size[0] = "medium";
-        }
-    }
-
-    private void turnItemLeft(){
+    public void turnItemLeft(){
         System.out.println("Item turned left!");
     }
 
-    private void turnItemRight(){
+    public void turnItemRight(){
         System.out.println("Item turned right!");
     }
 
-    private void turnItemUp(){
+    public void turnItemUp(){
         System.out.println("Item turned up!");
     }
 
-    private void turnItemDown(){
+    public void turnItemDown(){
         System.out.println("Item turned down!");
     }
 
@@ -164,12 +225,22 @@ public class Person implements Contract {
     /** 
      * @param item
      */
-    private void examine(String item){
+    public void examine(String item){
         System.out.println("You are examining the " + item + ".");
         turnItemDown();
         turnItemUp();
         turnItemLeft();
         turnItemRight();
+    }
+
+    public static void main(String[] args){
+        Person player1 = new Person("Steve");//create a new person named Steve
+        player1.grow();//grow Steve
+        player1.grow();//grow Steve
+        player1.shrink();//shrink Steve
+        player1.shrink();//shrink Steve
+        player1.shrink();//shrink Steve
+        player1.fly(20,30);//fly Steve to 20, 30
     }
 
 
--- reflection.md
+++ reflection.md
@@ -1,5 +1,34 @@
-Use this file to record your reflection on this assignment.
+<p align="center">
+  <a href="https://github.com/DenverCoder1/readme-typing-svg"><img src="https://readme-typing-svg.herokuapp.com?lines=meinKraft&center=true&width=500&height=50"></a>
+</p>
+<p>
+Abby McIlraith and Michelle Lawson are german scientists building a pirated version of a certain Swedish game that cannot be accessed in our country. We implement the Contract interface created by Jordan Crouser.
+</p>
 
-- Tell me about your class! What does it represent?
-- What additional methods (if any) did you implement alongside those listed in the interface?
-- What worked, what didn't, what advice would you give someone taking this course in the future?
+
+<h3>Brainstorming Ideas:</h3>
+
+    attributes?
+    - game inventory -> hashTable < String itemName, int numAvailable >
+        - content : hashTable
+        - capacity - do we really want it?
+    - coordinates for location - arr  int [x, y, z]
+    - size String [tiny, small, medium, large, giant]
+    - Boolean awake [either awake or asleep T/F]
+
+    methods
+    - grab from inventory
+    - drop item in inventory
+    - examine [turn left, turn right, turn front, turn to back (all print "You have turned this to the ___")]
+    - use - deduct from inventory
+    - walk - change coordinates (adds/subtracts x,y)
+    - fly - changes to 1, changes x,y accordingly, changes z back to 0
+    - shrink change size (with limitations)
+    - grow chanvfjedfvbjrfbd yeah
+    - rest change awake status
+    -undo (unRest!)? print "previous action undone!"
+
+Process:
+
+
+
